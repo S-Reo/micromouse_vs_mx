eeabc04170695df2bbddebd462e4fe0d5321c97e
S-Reo <Csrengin115it@gmail.com>
2021-10-03 14:47:21 +0900

    init

---
+++ Core/Inc/Control.h
@@ -0,0 +1,31 @@
+/*
+ * Control.h
+ *
+ *  Created on: 2021/09/29
+ *      Author: leopi
+ */
+
+#ifndef INC_CONTROL_H_
+#define INC_CONTROL_H_
+
+void Side_Wall_Control(float target, float now,float T, float KP, float KI, float KD);
+
+void Left_Wall_Control(float target, float now,float T, float KP, float KI, float KD);
+
+void Right_Wall_Control(float target, float now,float T, float KP, float KI, float KD);
+
+
+
+void Velocity_Control(float target, float now, float T, float KP, float KI, float KD);
+
+void Right_Velo_Control(float target, float now, float T, float KP, float KI, float KD);
+
+void Left_Velo_Control(float target, float now, float T, float KP, float KI, float KD);
+
+void Rotate_Control(float target, float T, float KP, float KI, float KD);
+
+void Enc_Velo_Control(float T, float KP, float KI, float KD);
+
+void Rad_Velo_Control(double target, double data, double T, float KP, float KI, float KD);
+
+#endif /* INC_CONTROL_H_ */
