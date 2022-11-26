896e224f0db3a6247cd95213763b42f53bc73cc6
S-Reo <csrengin115it@gmail.com>
2022-11-26 14:57:54 +0900

    試走会_完走ならず_ホコリ+時間

--- Core/Inc/MazeLib.h
+++ Core/Inc/MazeLib.h
@@ -22,8 +22,8 @@
 #define GOAL_SIZE_X 2
 #define GOAL_SIZE_Y 2
 
-#define GOAL_X 4
-#define GOAL_Y 4
+#define GOAL_X 9
+#define GOAL_Y 9
 
 #define __JUDGE_GOAL__(x,y) (( (GOAL_X <= x) && (x < GOAL_X + GOAL_SIZE_X)) && ((GOAL_Y <= y) && (y < GOAL_Y + GOAL_SIZE_Y)) )
 
